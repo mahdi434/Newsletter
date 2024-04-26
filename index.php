8e78728474ad640b366dbb1edf4a83b24e747db6
mahdi434 <119471660+mahdi434@users.noreply.github.com>
2024-04-26 20:13:19 +0300

    edit color

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+    <title>Sign in & Sign up Form</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="forms-container">
+            <div class="signin-signup">
+                <form action="login" class="sign-in-form" method="post">
+                    <h2 class="title">login in</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" name="username" placeholder="Username" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" name="password" placeholder="Password" />
+                    </div>
+                    <input type="submit" value="Login" class="btn solid" />
+
+                </form>
+                <form action="#" class="sign-up-form">
+                    <h2 class="title">Sign up</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text"  placeholder="Username" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-envelope"></i>
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <input type="submit" class="btn" value="Sign up" />
+                    <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="panels-container">
+            <div class="panel left-panel">
+                    
+                    <button class="btn transparent" id="sign-up-btn">
+                        Sign up
+                    </button>
+                </div>
+                <img src="" class="image" alt="" />
+            </div>
+            <div class="panel right-panel">
+                <div class="content">
+                    <h3>One of us ?</h3>
+                    
+                    <button class="btn transparent" id="sign-in-btn">
+                        Sign in
+                    </button>
+                </div>
+                <img src="" class="image" alt="" />
+            </div>
+        </div>
+    </div>
+
+    <script src="app.js"></script>
+</body>
+<?php
+
+session_start();
+//if(isset($_SESSION['uId'])){
+   // echo "<script> location.assign('index.php'); </script>";
+
+//}
+
+  $con = mysqli_connect("localhost","root","","newslitter");
+  $query = "SELECT * FROM `football`";
+  $result = mysqli_query($con,$query);
+  if(isset($_POST['login'])){
+    $msg = 0;
+    while($row = mysqli_fetch_array($result)){
+      if($_POST['username']==$row['uName'] &&$_POST['password']==$row['uPass']){
+        $msg=1;
+        $_SESSION['email']=$row['uEmail'];
+       
+      }
+    }
+    if($msg==1){
+      echo "<script> alert('welcome back : ".$_POST['email']."')</script>" ;
+    }else{
+      echo "<script> alert('wrong username or password !') </script>" ;
+    }
+  }
+?>
+</html>
